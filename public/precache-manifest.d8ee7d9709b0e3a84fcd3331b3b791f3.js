f8a806eec18cbed1ea5a805e778dccfd6a7d3ef6
lmxyjy <yjy15680489038@163.com>
2019-12-06 16:46:18 +0800

    first commit

---
+++ public/precache-manifest.d8ee7d9709b0e3a84fcd3331b3b791f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ae2fc2d25cea2bb3d2522ac940461f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eccea30a8ccae88bf0a",
+    "url": "/static/css/2.4e6f2c6b.chunk.css"
+  },
+  {
+    "revision": "bc3a9929d86c19890b7f",
+    "url": "/static/css/main.cec015f0.chunk.css"
+  },
+  {
+    "revision": "0eccea30a8ccae88bf0a",
+    "url": "/static/js/2.11eb1e0c.chunk.js"
+  },
+  {
+    "revision": "bc3a9929d86c19890b7f",
+    "url": "/static/js/main.50714327.chunk.js"
+  },
+  {
+    "revision": "82ea9f50d8e15c7d6b79",
+    "url": "/static/js/runtime-main.ed050b82.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "8043cf2a42b5b58bc20bf06bbbd8c911",
+    "url": "/static/media/bg.8043cf2a.jpg"
+  }
+]);
